e89aec569627468744f63a7af80959e7b2665b30
Trust Onyekwere <trustonyekwere151@gmail.com>
2026-09-14 19:12:09 +0100

    updated card UI

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Page() {
         <div className="flex min-h-screen justify-center p-12">
             <div className="text-center">
                 <p>Our Programmes</p>
-                <h1 className="font-black text-4xl pt-3 pb-6">Make A Difference</h1>
+                <h1 className="font-black text-gray-800 dark:text-gray-200 text-4xl pt-3 pb-6">Make A Difference</h1>
                 <div className="grid md:grid-cols-2 flex-col min-w-3xl gap-6">
-                    <Card className="relative mx-auto w-full max-w-sm pt-0 hover:scale-[1.02] transition-all duration-300">
+                    <Card className="relative mx-auto w-full hover:bg-emerald-50 dark:bg-[#171717] max-w-sm pt-0 hover:scale-[1.02] transition-all duration-300">
                         <div className="absolute inset-0 z-30 aspect-video" />
                         <img
                             src="/football.jpg"
@@ -22,15 +22,13 @@ export default function Page() {
                             className="relative z-20 aspect-video w-full object-cover brightness-80 dark:brightness-60 will-change-transform"
                         />
                         <CardHeader>
-                            <CardTitle>Football</CardTitle>
+                            <CardTitle>Football Camps</CardTitle>
                             <CardDescription>
-                                Camps, skills, 
-                                discipline and 
-                                teamwork 
+                                Camps, specialized skills, rigorous discipline, and seamless teamwork
                             </CardDescription>
                         </CardHeader>
                     </Card>
-                    <Card className="relative mx-auto w-full max-w-sm pt-0 hover:scale-[1.02] transition-all duration-300">
+                    <Card className="relative mx-auto w-full hover:bg-emerald-50 dark:bg-[#171717] max-w-sm pt-0 hover:scale-[1.02] transition-all duration-300">
                         <div className="absolute inset-0 z-30 aspect-video" />
                         <img
                             src="mentorship.jpg"
@@ -44,7 +42,7 @@ export default function Page() {
                             </CardDescription>
                         </CardHeader>
                     </Card>
-                    <Card className="relative mx-auto w-full max-w-sm pt-0 hover:scale-[1.02] transition-all duration-300">
+                    <Card className="relative mx-auto w-full hover:bg-emerald-50 dark:bg-[#171717] max-w-sm pt-0 hover:scale-[1.02] transition-all duration-300">
                         <div className="absolute inset-0 z-30 aspect-video" />
                         <img
                             src="education.jpg"
@@ -58,7 +56,7 @@ export default function Page() {
                             </CardDescription>
                         </CardHeader>
                     </Card>
-                    <Card className="relative mx-auto w-full max-w-sm pt-0 hover:scale-[1.02] transition-all duration-300">
+                    <Card className="relative mx-auto w-full hover:bg-emerald-50 dark:bg-[#171717] max-w-sm pt-0 hover:scale-[1.02] transition-all duration-300">
                         <div className="absolute inset-0 z-30 aspect-video" />
                         <img
                             src="community.jpg"
