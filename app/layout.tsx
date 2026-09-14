76be9403e8e720906dc2abce73e2da40316c2097
Trust Onyekwere <trustonyekwere151@gmail.com>
2026-09-14 18:32:04 +0100

    added Nunito font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,29 @@
-import { Geist, Geist_Mono, Inter, Nunito_Sans } from "next/font/google"
+import { Nunito } from 'next/font/google';
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
 
-const nunitoSansHeading = Nunito_Sans({subsets:['latin'],variable:'--font-heading'});
-
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+// font config
+const nunito = Nunito({
+    subsets: ['latin'],
+    variable: '--font-nunito', // Define the CSS variable
+    display: 'swap',
+});
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable, nunitoSansHeading.variable)}
-    >
-      <body>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
-    </html>
-  )
+    return (
+        <html
+        lang="en"
+        suppressHydrationWarning
+        className={nunito.variable}
+        >
+            <body>
+                <ThemeProvider>{children}</ThemeProvider>
+            </body>
+        </html>
+    )
 }
