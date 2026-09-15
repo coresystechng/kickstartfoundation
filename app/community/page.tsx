7b144a64e3d6d9c329dc12edc0b5abd335f3f079
Trust Onyekwere <trustonyekwere151@gmail.com>
2026-09-16 00:07:10 +0100

    updated details pages UI

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -3,32 +3,32 @@ import Image from 'next/image';
 export default function Page() {
     return (
         <>
-            <div className="relative mx-auto w-full max-w-4xl min-h-screen pt-0">
+            <div className="relative mx-auto w-full max-w-4xl min-h-screen pt-10">
                 <img
                     src="community.jpg"
                     alt="Event cover"
                     className="relative z-20 max-h-100 w-full object-cover brightness-80 dark:brightness-60 will-change-transform"
                 />
-                <div className="pt-10">
-                    <div className="flex items-center gap-2">
+                <div className="pt-8">
+                    <div className="flex items-center gap-2 px-2 py-2 mb-4 bg-gray-200">
                         <Image
                             src="/community.gif"
-                            width={60}
-                            height={60}
+                            width={55}
+                            height={55}
                             alt="Community Engagement"
                             unoptimized // Keep this to ensure the GIF animates
                         />
-                        <h1 className="font-black text-5xl">Community Engagement</h1>
+                        <h1 className="font-black text-4xl dark:text-gray-900">Community Engagement</h1>
                     </div>
+                    <p className='text-neutral-400'>Building stronger young people through stronger communities.</p>
+                    <div className='bg-emerald-200 h-0.5 w-[450] rounded' />
                     <div>
-                    <p className="pt-4 text-lg">
-                        Building stronger young people through stronger communities. <br />
-
+                    <p className="pt-4 text-lg text-neutral-600 dark:text-neutral-300">
                         Young people thrive when they feel supported, connected and valued. Through community events, outreach initiatives and partnerships, KICKSTEP brings families, mentors, organisations and supporters together to create environments where young people have opportunities to learn, participate and grow. <br />
                     </p>
 
-                    <div className="pt-4 text-lg">
-                        <span className="font-bold text-3xl text-gray-900 dark:text-gray-300">Focus: <br /></span>
+                    <div className="pt-4 text-lg text-neutral-600 dark:text-neutral-300">
+                        <span className="font-semibold text-2xl text-gray-900 dark:text-white">Focus: <br /></span>
                         <ol className="pt-2 pb-8">
                             <li>
                                 • Community events and outreach
--- app/education/page.tsx
+++ app/education/page.tsx
@@ -3,32 +3,32 @@ import Image from 'next/image';
 export default function Page() {
     return (
         <>
-            <div className="relative mx-auto w-full max-w-4xl min-h-screen pt-0">
+            <div className="relative mx-auto w-full max-w-4xl min-h-screen pt-10">
                 <img
                     src="education.jpg"
                     alt="Event cover"
                     className="relative z-20 max-h-100 w-full object-cover brightness-80 dark:brightness-60 will-change-transform"
                 />
-                <div className="pt-10">
-                    <div className="flex items-center gap-2">
+                <div className="pt-8">
+                    <div className="flex items-center gap-2 px-2 py-2 mb-4 bg-gray-200">
                         <Image
                             src="/education.gif"
-                            width={60}
-                            height={60}
+                            width={55}
+                            height={55}
                             alt="Education"
                             unoptimized // Keep this to ensure the GIF animates
                         />
-                        <h1 className="font-black text-5xl">Education</h1>
+                        <h1 className="font-black text-4xl dark:text-gray-900">Education</h1>
                     </div>
+                    <p className='text-neutral-400'>Creating opportunities for learning and future growth.</p>
+                    <div className='bg-emerald-200 h-0.5 w-96 rounded' />
                     <div>
-                    <p className="pt-4 text-lg">
-                        Creating opportunities for learning and future growth. <br />
-
+                    <p className="pt-4 text-lg text-neutral-600 dark:text-neutral-300">
                         KICKSTEP recognises that long-term success extends beyond sporting ability. Through educational initiatives, learning opportunities and exposure to useful resources, the Foundation encourages young people to value education, develop their potential and prepare for opportunities beyond football.<br />
                     </p>
 
-                    <div className="pt-4 text-lg">
-                        <span className="font-bold text-3xl text-gray-900 dark:text-gray-300">Focus: <br /></span>
+                    <div className="pt-4 text-lg text-neutral-600 dark:text-neutral-300">
+                        <span className="font-semibold text-2xl text-gray-900 dark:text-white">Focus: <br /></span>
                         <ol className="pt-2 pb-8">
                             <li>
                                 • Encouraging academic development
